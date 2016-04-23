111f232321477c520815a261454db49f2a595fd8
yspark <ysparknanum@gmail.com>
2016-04-23 22:50:26 +0900

    fix bug

---
+++ Tutorial/Tutorial/index.js
@@ -0,0 +1,10 @@
+﻿var server = require('./server.js');
+var router = require('./router.js');
+var requestHandler = require('./requestHandler.js');
+
+var handlers = {};
+handlers['/'] = requestHandler.start;
+handlers['/start'] = requestHandler.start;
+handlers['/upload'] = requestHandler.upload;
+
+server.start(router.route, handlers);
